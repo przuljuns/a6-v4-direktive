74dcc7d05346986273a23cc7c047db98671a0b60
djordjeprzulj <przulj@gmail.com>
2018-10-10 23:13:30 +0200

    obrisan encapsulation

--- src/app/quiz-item-form/quiz-item-form.component.ts
+++ src/app/quiz-item-form/quiz-item-form.component.ts
@@ -5,8 +5,6 @@ import { QuizItem } from '../model/quiz-item.model';
   selector: 'app-quiz-item-form',
   templateUrl: './quiz-item-form.component.html',
   styleUrls: ['./quiz-item-form.component.css']
-  //, encapsulation: ViewEncapsulation.None
-  //, encapsulation: ViewEncapsulation.ShadowDom
 })
 export class QuizItemFormComponent implements OnInit {
   @Output() quizItemCreated = new EventEmitter<QuizItem>();
