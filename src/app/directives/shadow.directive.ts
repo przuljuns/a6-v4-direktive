34a648a512a037a85f2a7233cb6d72d0a965c2ce
djordjeprzulj <przulj@gmail.com>
2018-10-11 00:14:50 +0200

    implementacija shadow

--- src/app/directives/shadow.directive.ts
+++ src/app/directives/shadow.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appShadow]'
 })
 export class ShadowDirective {
 
-  constructor() { }
-
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) { 
+    renderer.setStyle(elementRef.nativeElement, 'box-shadow', '2px 2px 12px blue');
+  }
 }
